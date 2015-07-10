de0fc6ade81d3b03313a32ad860895a363ab268f
Adegeye Mayowa <mayox4ever2006@gmail.com>
2015-07-10 09:25:09 +0100

    final

--- mj.js
+++ mj.js
@@ -1,9 +1,15 @@
 $(document).ready( function () {
-  $.ajax({
-  url: "https://api.github.com/search/users?q=tom+repos:%3E42+followers:%3E1000" ,
-  dataType: 'json',
-  success: function(results){
-    console.log(results);
+  $('#button_click').click(function () {  
+    var form_value = $("#USERNAME").val();
+    console.log(form_value);
+    $.ajax({
+    url: "https://api.github.com/users/" + form_value ,
+    dataType: 'json',
+    success: function(results){
+      console.log(results.login);
+        var url_go = "https://github.com/" + form_value;
+      window.location.href= url_go;
   }
+  }); 
 });
-})
+});
